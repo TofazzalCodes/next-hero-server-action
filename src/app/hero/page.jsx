e7d7bb4d762e9ef49038896cbce8938063aacf46
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-09-13 09:57:28 +0600

    introduction heroui

--- src/app/hero/page.jsx
+++ src/app/hero/page.jsx
@@ -6,6 +6,9 @@ const HeroPage = () => {
         <div>
             <h2>Hero page</h2>
             <Button>Hero Button</Button>
+            <Button variant="secondary">Hero Button</Button>
+            <Button variant="danger-soft">Hero Button</Button>
+            <Button variant="danger">Hero Button</Button>
         </div>
     );
 };
