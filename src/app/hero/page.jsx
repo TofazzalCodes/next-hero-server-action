be33de95e93fb57242f67c52c1c49decb25c33dc
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-09-13 09:50:10 +0600

    HeroUi set up and learn

---
+++ src/app/hero/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@heroui/react";
+
+
+const HeroPage = () => {
+    return (
+        <div>
+            <h2>Hero page</h2>
+            <Button>Hero Button</Button>
+        </div>
+    );
+};
+
+export default HeroPage;
